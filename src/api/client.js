3cb3643dce8ca56f1b46abde696a86eb9356126a
Ruben Lupancu <lupancuruben2@gmail.com>
2026-01-13 17:39:43 +0200

    Polish Design-mobile first 2

--- src/api/client.js
+++ src/api/client.js
@@ -1,4 +1,4 @@
-const API_URL = "http://localhost:3000";
+const API_URL = "https://bucovinastay-backend-2.onrender.com"  ;
 
 let maintenanceListeners = new Set();
 let maintenanceActive = false; // ✅ latch
